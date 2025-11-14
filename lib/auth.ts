57ccfc1591e3a46b9c3172ab06407e9f18dd0165
sahaarindia <sahaarindia@gmail.com>
2025-11-14 18:26:52 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
-import { AuthOptions } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
