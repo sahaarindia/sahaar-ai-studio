8e89874cc7b3815423e25ac2ed23cc90dd3e800b
sahaarindia <sahaarindia@gmail.com>
2025-11-14 17:39:13 +0530

    Create pdf-parse.d.ts

---
+++ types/pdf-parse.d.ts
@@ -0,0 +1,31 @@
+declare module 'pdf-parse' {
+  interface PDFInfo {
+    PDFFormatVersion: string;
+    IsAcroFormPresent: boolean;
+    IsXFAPresent: boolean;
+    Title?: string;
+    Author?: string;
+    Subject?: string;
+    Creator?: string;
+    Producer?: string;
+    CreationDate?: string;
+    ModDate?: string;
+  }
+
+  interface PDFMetadata {
+    [key: string]: any;
+  }
+
+  interface PDFData {
+    numpages: number;
+    numrender: number;
+    info: PDFInfo;
+    metadata: PDFMetadata | null;
+    text: string;
+    version: string;
+  }
+
+  function pdf(dataBuffer: Buffer): Promise<PDFData>;
+  
+  export = pdf;
+}
