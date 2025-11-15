0517be5708743201199fadd98b2eb498ad89b96b
sahaarindia <sahaarindia@gmail.com>
2025-11-15 13:44:07 +0530

    Update next.config.mjs
    fix: correct Response body type

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {},
+  
   webpack: (config, { isServer }) => {
     config.experiments = {
       ...config.experiments,
